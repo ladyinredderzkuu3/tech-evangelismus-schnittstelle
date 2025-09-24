0aea7716abb22abd9c0b68fff427bdd707574293
ladyinredderzkuu3 <mwineesref@hotmail.com>
2025-09-24 09:00:40 -0500

    Updated welcome message for clarity.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Tech Evangelism Interface!');
+    res.send('Welcome to the Tech Evangelism Interface! Explore our APIs.');
 });
 
 app.listen(PORT, () => {
